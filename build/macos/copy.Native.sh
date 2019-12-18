f6c870ca8a45d74b09407ce95f64845da5803643
Dirk Lemstra <dirk@lemstra.org>
2019-12-18 08:06:15 +0100

    Added GitHub actions build.

--- build/macos/copy.Native.sh
+++ build/macos/copy.Native.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+[ ! -d "$1" ] && mkdir $1
+
 cp libHeif.Native.dll.dylib $1/Heif.Native.dll.dylib
 
 mkdir -p ../../tests/Heif.Tests/bin/Release/netcoreapp2.0
--- build/windows/copy.Heif.cmd
+++ build/windows/copy.Heif.cmd
@@ -1,5 +1,9 @@
 @echo off
 
+IF NOT EXIST "%1" (
+    mkdir "%1"
+)
+
 cd ..\..\tests\Heif.Tests
 
 copy bin\x64\Release\netcoreapp2.0\Heif.dll "%1"
