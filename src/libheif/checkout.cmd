dbe745972b2b48a3bc3f4fe86cb19fbdc61d9c76
Jason Nelson <jason@carbonmade.com>
2020-03-10 14:13:38 -0700

    Revert "Update libheif to v1.6.2"
    This reverts commit 8946f71af0b46bfd0f40688e968baab185b2f384.

--- src/libheif/checkout.cmd
+++ src/libheif/checkout.cmd
@@ -4,7 +4,7 @@ if not exist code (
     git clone https://github.com/strukturag/libheif code
     cd code
     git pull
-    git checkout tags/v1.6.2
+    git checkout tags/v1.6.1
     copy /Y ..\CMakeLists.windows.txt libheif\CMakeLists.txt
     cmake . -D CMAKE_GENERATOR_PLATFORM=x64 -D LIBDE265_FOUND=1 -D LIBDE265_CFLAGS="-I ..\..\..\libde265\code" -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_DEBUG="/MTd"
     cd ..
--- src/libheif/checkout.sh
+++ src/libheif/checkout.sh
@@ -3,4 +3,4 @@ set -e
 
 git clone https://github.com/strukturag/libheif code
 cd code
-git checkout tags/v1.6.2
+git checkout tags/v1.6.1
