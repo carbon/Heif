a497d6689c30ac5367d0416ba63a3e894fb2df3d
Jason Nelson <jason@carbonmade.com>
2020-04-30 13:39:40 -0700

    Fix path of new DLL

--- build/windows/copy.Heif.cmd
+++ build/windows/copy.Heif.cmd
@@ -6,5 +6,5 @@ IF NOT EXIST "%1" (
 
 cd ..\..\tests\Heif.Tests
 
-copy bin\x64\Release\netcoreapp2.0\Heif.dll "%1"
+copy bin\x64\Release\netcoreapp2.0\Carbon.Codecs.Heif.dll "%1"
 copy bin\x64\Release\netcoreapp2.0\Heif.Native.dll "%1"
