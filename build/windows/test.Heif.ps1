0af58f3a6e2ac5cd32da9eb7826d11107e76fe9b
Dirk Lemstra <dirk@lemstra.org>
2020-05-05 10:36:37 +0200

    Corrected library name.

--- build/windows/test.Heif.ps1
+++ build/windows/test.Heif.ps1
@@ -8,7 +8,7 @@ function runTests()
     $vstest = "$($env:VSINSTALLDIR)\Common7\IDE\Extensions\TestPlatform\vstest.console.exe"
 
     $folder = fullPath "tests\Heif.Tests\bin\x64\Release\netcoreapp2.0"
-    $fileName = "$folder\Heif.Tests.dll"
+    $fileName = "$folder\Carbon.Codecs.Heif.Tests.dll"
 
     & $vstest $fileName /platform:x64 /TestAdapterPath:$folder
 
