0e249d2573f9e8a977266b10c1e18884ef69a699
Dirk Lemstra <dirk@lemstra.org>
2020-06-21 12:25:13 +0200

    Updated libheif.

--- src/libheif/checkout.cmd
+++ src/libheif/checkout.cmd
@@ -1,10 +1,10 @@
 @echo off
 
 if not exist code (
-    git clone https://github.com/strukturag/libheif code
+    git clone https://github.com/ImageMagick/libheif code
     cd code
     git pull
-    git checkout tags/v1.6.1
+    git checkout 45145e3a45f49975b48c027982d8e629ee2acddb
     copy /Y ..\CMakeLists.windows.txt libheif\CMakeLists.txt
     cmake . -D CMAKE_GENERATOR_PLATFORM=x64 -D LIBDE265_FOUND=1 -D LIBDE265_CFLAGS="-I ..\..\..\libde265\code" -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_DEBUG="/MTd"
     cd ..
