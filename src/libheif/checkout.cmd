7f382594e469d5a5c5b492403ad6da5fddd88aa5
Dirk Lemstra <dirk@lemstra.org>
2021-09-20 22:04:42 +0200

    Patches due to changes in the CMake files.

--- src/libheif/checkout.cmd
+++ src/libheif/checkout.cmd
@@ -6,6 +6,6 @@ if not exist code (
     git pull
     git checkout e106504e3bf32882c8fc89972e22bcdc816a11b4
     copy /Y ..\CMakeLists.windows.txt libheif\CMakeLists.txt
-    cmake . -D CMAKE_GENERATOR_PLATFORM=x64 -D LIBDE265_FOUND=1 -D LIBDE265_CFLAGS="-I ..\..\..\libde265\code" -D AOM_FOUND=1 -D AOM_CFLAGS="-I ..\..\..\aom\code" -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_DEBUG="/MTd"
+    cmake . -D CMAKE_GENERATOR_PLATFORM=x64 -D CUSTOM_INCLUDE_DIRS="..\..\..\libde265\code;..\..\..\aom\code" -D CMAKE_CXX_FLAGS_RELEASE="/MT" -D CMAKE_CXX_FLAGS_DEBUG="/MTd"
     cd ..
 )
