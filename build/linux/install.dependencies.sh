2906be5008f0886a9be81d5cc06a85fecd1670fa
Dirk Lemstra <dirk@lemstra.org>
2021-10-08 07:56:51 +0200

    Install cmake through pip3 instead to make sure a recent version is installed.

--- build/linux/install.dependencies.sh
+++ build/linux/install.dependencies.sh
@@ -3,4 +3,7 @@ set -e
 
 apt-get update -y
 
-apt-get install -y autoconf cmake git libtool pkg-config
+apt-get install -y autoconf git libtool pkg-config python3-pip
+
+pip3 install cmake
+cmake --version
